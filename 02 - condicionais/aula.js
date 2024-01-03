72889e4e75825d51a05f5deabe536e1ab7f3f1e4
Andre S Nascimento <andresoaresnascimento@gmail.com>
2024-01-03 15:25:40 -0300

    if else

--- 02 - condicionais/aula.js
+++ 02 - condicionais/aula.js
@@ -1,5 +1,14 @@
 
-const numero = 11;
-const eNumeroPar = (numero % 2) === 0; 
+const numero = 6;
+const numeroDivisivelPor5 = (numero % 5) === 0; 
+//console.log(numeroPar);
 
-console.log(eNumeroPar);
+if(numero === 0){
+    console.log('O número é invalido');
+} else if (numero === 5) {
+    console.log('O número é invalido');
+} else if(numeroDivisivelPor5){
+    console.log('Sim');
+} else {
+    console.log('Não');
+}
